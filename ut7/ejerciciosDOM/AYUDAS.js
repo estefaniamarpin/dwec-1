a93c68c237e7bd7832c70b8b1027aef61b306154
estefaniamarpin <estefaniamarpin@gmail.com>
2021-12-20 01:55:38 +0100

    .

---
+++ ut7/ejerciciosDOM/AYUDAS.js
@@ -0,0 +1,10 @@
+
+AYUDA PARA EL EJERCICIO 2: MOD-modTexto.html
+// SELECCIONAR EL SELECT
+let miSelect = document.getElementById("tam");
+
+// OBTENER EL ÍNDICE DEL OPTION SELECCIONADO
+let indiceOpcionSeleccionada = miSelect.selectedIndex
+
+// OBTENER EL VALUE DEL OPTION SELECCIONADO
+let valorOpcionSeleccionada = miSelect.options[indiceOpcionSeleccionada].value
